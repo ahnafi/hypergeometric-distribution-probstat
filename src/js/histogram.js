0a5415a036d6fd05064753bc8d5035fed9a1ba3c
ahnafi <devsulthon@gmail.com>
2024-05-05 18:26:01 +0700

    add(histogram.js): function to create histogram

---
+++ src/js/histogram.js
@@ -0,0 +1,82 @@
+import { createLine, createRect, createText } from "./dom.js";
+import { hypergeometric } from "./math.js";
+
+const svg = document.getElementById("histogram");
+
+let widthBar = 30;
+let spaceBar = 1;
+let svgWidth = 600; //default
+let svgHeight = 200; //default
+let frekuensi = 0.0;
+let hightFreq = 0;
+let xbar = [];
+let prob = [];
+const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+function createHistogram(x, N, n, m) {
+  // mencari propabilitas
+  for (let i = 0; i <= n; i++) {
+    prob.push(hypergeometric(i, N, n, m));
+  }
+
+  // mencari prob tertinggi untuk mengukur tinggi svg
+  for (let i = 0; i < prob.length - 1; i++) {
+    if (prob[i] < prob[i + 1]) {
+      hightFreq = prob[i + 1];
+    }
+  }
+  // debug
+  console.log("frekuensi tertinggi : " + hightFreq);
+
+  // mengubah tinggi svg
+  svgHeight = ((hightFreq * 100)/4 )* 20 + margin.bottom + margin.top ;
+  
+  //   membuat ukuran frekuensi
+  let marginFreq = svgHeight - margin.bottom;
+  let sizeFreq = (hightFreq * 100) / 4;
+  for (let i = 0; i < sizeFreq; i++) {
+    createText(margin.right - 2, marginFreq, `${frekuensi.toFixed(2)}`);
+    frekuensi += 0.05;
+    marginFreq -= 20;
+  }
+
+  //   mencari koordinat x setiap bar
+  for (let i = 0; i <= prob.length; i++) {
+    if (i == 0) {
+      xbar[i] = margin.left + spaceBar;
+    } else {
+      xbar[i] = xbar[i - 1] + widthBar + spaceBar;
+    }
+  }
+
+  //   membuat bar
+  for (let i = 0; i < prob.length; i++) {
+    let barHeight = prob[i] * 100 * 4;
+    let yBar = svgHeight - margin.bottom - barHeight;
+    // debug
+    console.log(`indeks ke ${i} ` + prob[i]  );
+
+    if (i == x) {
+      createRect(xbar[i], yBar, widthBar, barHeight, i, "red");
+    } else {
+      createRect(xbar[i], yBar, widthBar, barHeight, i);
+    }
+  }
+
+  // lebar svg
+  svgWidth = xbar.pop() + margin.right;
+  
+  svg.setAttribute("width", svgWidth);
+  svg.setAttribute("height", svgHeight);
+
+  //membuat garis
+  createLine(
+    margin.left,
+    svgWidth - margin.right,
+    svgHeight - margin.bottom,
+    svgHeight - margin.bottom
+  ); //x
+  createLine(margin.left, margin.left, margin.top, svgHeight - margin.bottom); //y
+}
+
+export { createHistogram };
