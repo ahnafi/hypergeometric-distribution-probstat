a38dcf1d8c35a3b3befae198a647d2a9328269c4
ahnafi <devsulthon@gmail.com>
2024-05-07 07:47:51 +0700

    add(main.js): value input ->  casting to number
    and move variabel scope xsamex to bottom

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,15 @@ const xsamex = document.getElementById("xsample");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-{
+
+  const xx = Number(x.value);
+  const NN = Number(N.value);
+  const nn = Number(n.value);
+  const mm = Number(m.value);
+
+  createHistogram(xx, NN, nn, mm);
+
+  //
   const xBigInt = BigInt(x.value);
   const NBigInt = BigInt(N.value);
   const nBigInt = BigInt(n.value);
@@ -19,9 +27,4 @@ form.addEventListener("submit", (e) => {
 
   const resultBigInt = hypergeometric(xBigInt, NBigInt, nBigInt, mBigInt);
   xsamex.value = Number(resultBigInt.toString());
-}
-
-  createHistogram(x.value, N.value, n.value, m.value);
-
-  console.log(resultBigInt.toString());
 });
